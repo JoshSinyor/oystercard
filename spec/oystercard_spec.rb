1495ab7b2bff562e74bc170d583e0df7e216a369
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-11 15:35:46 +0000

    User can now top up their balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,14 @@ describe Oystercard do
   it 'starts with a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  describe 'top up' do
+    it 'checks if the oystercard responds to the top_up method' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'Adds money to balance' do
+      expect{ subject.top_up(50) }.to change{ subject.balance }.by 50
+    end
+  end
 end
